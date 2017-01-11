72e441c2bca9e9cb105caa4c28b4439eedfa93a5
neilchikani <chikaninilay@gmail.com>
2017-01-11 13:39:45 +0530

    started with react redux

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,19 +1,12 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
-import createLogger from 'redux-logger';
 import allReducers from './reducers';
+const store = createStore(allReducers);
 import App from './components/App';
 
-const logger = createLogger();
-const store = createStore(
-    allReducers,
-    applyMiddleware(thunk, promise, logger)
-);
 
 ReactDOM.render(
     <Provider store={store}>
@@ -21,3 +14,19 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
+
+
+// import {createStore, applyMiddleware} from 'redux';
+// import thunk from 'redux-thunk';
+// import promise from 'redux-promise';
+// import createLogger from 'redux-logger';
+
+
+//
+// const logger = createLogger();
+// const store = createStore(
+//     allReducers,
+//     applyMiddleware(thunk, promise, logger)
+// );
+//
+
