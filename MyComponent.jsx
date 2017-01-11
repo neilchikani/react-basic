d02af44fb6a3675e71d9e5c2e78da55986de6ed5
neilchikani <chikaninilay@gmail.com>
2017-01-11 11:00:05 +0530

    changes

--- MyComponent.jsx
+++ MyComponent.jsx
@@ -2,37 +2,40 @@ import React from 'react';
 var MyComponent = React.createClass({
   getInitialState: function () {
     return {
-     editing: false
+      editing: false
     }
   },
-  editForm: function(){
-    this.setState({ editing: true })
+  editForm: function () {
+    this.setState({editing: true})
   },
   save: function () {
-    this.props.children = this.refs.myTextArea.value;
-    this.setState({ editing: false })
+    this.props.editCommentText(this.refs.myTextArea.value, this.props.index);
+    this.setState({editing: false})
+  },
+  removeForm: function () {
+    this.props.removeCommentText(this.props.index);
   },
   showEditForm: function () {
-    return(
+    return (
       <div>
         <textarea ref="myTextArea" defaultValue={this.props.children}></textarea>
-        <button onClick={this.save} type="submit"> Save </button>
+        <button onClick={this.save} type="submit"> Save</button>
       </div>
     )
   },
   showNormalForm: function () {
-    return(
+    return (
       <div>
         <h2> {this.props.children} </h2>
-        <button onClick={this.editForm} type="submit"> Edit </button>
-        <button type="submit"> Remove  </button>
+        <button onClick={this.editForm} type="submit"> Edit</button>
+        <button onClick={this.removeForm} type="submit"> Remove</button>
       </div>
     )
   },
   render: function () {
-    if(this.state.editing){
+    if (this.state.editing) {
       return this.showEditForm();
-    }else{
+    } else {
       return this.showNormalForm();
     }
   }
@@ -48,13 +51,30 @@ var BoardComponent = React.createClass({
       ]
     }
   },
+  addComment: function (text) {
+    var arr = this.state.comments;
+    arr.push(text);
+    this.setState({comments: arr});
+  },
+  editComment: function (newText, i) {
+    var arr = this.state.comments;
+    arr.splice(i, 1, newText);
+    this.setState({comments: arr});
+  },
+  removeComment: function (i) {
+    var arr = this.state.comments;
+    arr.splice(i, 1);
+    this.setState({comments: arr});
+  },
   render: function () {
-
+    var that = this;
     return (
       <div>
+        <button onClick={this.addComment.bind(null, "Hello")} type="submit"> Add Component</button>
         {
-          this.state.comments.map(function (item,i) {
-            return (<MyComponent key={i}> {item} </MyComponent>)
+          this.state.comments.map(function (item, i) {
+            return (<MyComponent key={i} index={i} removeCommentText={that.removeComment}
+                                 editCommentText={that.editComment}> {item} </MyComponent>)
           })
         }
       </div>
