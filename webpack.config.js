46cda0a605e42db1b5a25d17ea25f97c9f9b6ed7
Nilay Chikani <nilay.chikani@ii5.com>
2017-03-22 15:14:03 +0530

    updating configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,7 @@ var config = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react']
-        }
+        loader: 'babel'
       }
     ]
   }
